0e331501604616a720a8cc99d86f269a686a5ce7
Snehasish Das <141271526+Unknowncoder3@users.noreply.github.com>
2026-09-07 23:43:11 +0530

    feat: configure ASP.NET Core API and CORS

---
+++ backend/TrainOperations.Api/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Angular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("Angular");
+app.MapControllers();
+
+app.Run();
